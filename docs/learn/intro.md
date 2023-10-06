219b4ad57c4be66accae8b7f24931847da13af75
slhmy <1484836413@qq.com>
2023-10-06 10:43:09 +0800

    Config and content prepare

---
+++ docs/learn/intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# OJ Lab Learn Intro
+
+[WIP]
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,28 +1,28 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'OJ Lab Docusaurus',
-  tagline: 'Official content release for OJ Lab',
-  favicon: 'img/favicon.ico',
+  title: "OJ Lab Docusaurus",
+  tagline: "Official content release for OJ Lab",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: "https://your-docusaurus-test-site.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/oj-lab-docusaurus/',
+  baseUrl: "/oj-lab-docusaurus/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'OJ-lab', // Usually your GitHub org/user name.
-  projectName: 'oj-lab-docusaurus', // Usually your repo name.
+  organizationName: "OJ-lab", // Usually your GitHub org/user name.
+  projectName: "oj-lab-docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   trailingSlash: true,
 
@@ -30,31 +30,25 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/OJ-lab/oj-lab-docusaurus",
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: "https://github.com/OJ-lab/oj-lab-docusaurus",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -64,67 +58,73 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'OJ Lab Docusaurus',
+        title: "OJ Lab Docusaurus",
         logo: {
-          alt: 'OJ Lab Logo',
-          src: 'img/oj-lab-icon.svg',
+          alt: "OJ Lab Logo",
+          src: "img/oj-lab-icon.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            type: "docSidebar",
+            sidebarId: "learnSidebar",
+            position: "left",
+            label: "Learn",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/OJ-lab/oj-lab-docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            to: "/blog",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            type: "docSidebar",
+            sidebarId: "docusaurusSidebar",
+            position: "left",
+            label: "Docusaurus",
+          },
+          {
+            href: "https://github.com/OJ-lab/oj-lab-docusaurus",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "OJ Lab Learn",
+                to: "/docs/learn/intro",
+              },
+              {
+                label: "Docusaurus Tutorial",
+                to: "/docs/docusaurus/intro",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Discord",
+                href: "https://discord.gg/egXjPX7Tr3",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/OJ-lab/oj-lab-docusaurus",
               },
             ],
           },
